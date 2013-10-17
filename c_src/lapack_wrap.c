33855a1a6c38af66e433b3d0b2c18ac62854164f
Dmitry Davydov <haudvd@gmail.com>
2013-10-17 21:14:49 +0400

    add lapack library

--- c_src/lapack_wrap.c
+++ c_src/lapack_wrap.c
@@ -1,33 +1,42 @@
 #include "erl_nif.h"
+#include "lapacke.h"
 
-static ERL_NIF_TERM get(
-        ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
-    double load_avg[3];
-    int loads;
 
-    loads = getloadavg(load_avg, 3);
-    if(loads != 3) {
-        return enif_make_tuple(
-                env, 2,
-                enif_make_atom(env, "error"),
-                enif_make_atom(env, "invalid_result") 
-            );
-    }
+#define N 5 //должно быть равно количеству продуктов
+#define NRHS 1
+#define LDA N
+#define LDB NRHS
 
-    return enif_make_tuple(
-            env, 2,
-            enif_make_atom(env, "ok"),
-            enif_make_tuple(
-                env, 3,
-                enif_make_double(env, load_avg[0]),
-                enif_make_double(env, load_avg[1]),
-                enif_make_double(env, load_avg[2])
-            )
-        );
+void print_result( ErlNifEnv* env, lapack_int n, double* a ) {
+  lapack_int i, j; // в си вообще есть рефлексия?...
+  return enif_make_list(env, enif_make_double(env, a[0]), enif_make_double(env, a[1]));
+  // иначе повторять по числу продуктов
+}
+
+
+static ERL_NIF_TERM solve(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
+  double *a; 
+  double *b; 
+  enif_get_list_cell(env, argv[0], a, null);
+  
+  enif_get_list_cell(env, argv[1], b, null);
+  
+  lapack_int n = N, nrhs = NRHS, lda = LDA, ldb = LDB, info;
+  lapack_int ipiv[N];
+  info = LAPACKE_dgesv( LAPACK_ROW_MAJOR, n, nrhs, a, lda, ipiv, b, ldb );
+  
+  if( info > 0 ) {
+    printf( "The diagonal element of the triangular factor of A,\n" );
+    printf( "U(%i,%i) is zero, so that A is singular;\n", info, info );
+    printf( "the solution could not be computed.\n" );
+    exit( 1 );
+  }
+  
+  return create_matrix(n, nrhs, b, ldb);
 }
 
 static ErlNifFunc nif_funcs[] = {
-    {"get", 0, get}
+  {"solve", 2, solve}
 };
 
-ERL_NIF_INIT(load_average, nif_funcs, NULL, NULL, NULL, NULL)
+ERL_NIF_INIT(lapack_wrap, nif_funcs, NULL, NULL, NULL, NULL)
