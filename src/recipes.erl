5fe5b1a89a6f6c1518b05878a57b51f1a290c25e
Dmitry Davydov <haudvd@gmail.com>
2013-11-20 03:42:19 +0400

    fix utf-8

--- src/recipes.erl
+++ src/recipes.erl
@@ -16,15 +16,15 @@ image_hash(Link) ->
     string:to_lower(lists:flatten(End)).
 
 recipe_dummy() ->
-     #recipe{name = <<"Булки с чаем">>, time = <<"12 мин.">>, number_portion = 4, complexity = 1, ingredients = [<<"2 булки">>, <<"1 чай">>],
+     #recipe{name = <<"Булки с чаем"/utf8>>, time = <<"12 мин."/utf8>>, number_portion = 4, complexity = 1, ingredients = [<<"2 булки"/utf8>>, <<"1 чай"/utf8>>],
              steps_exists = true, how_to_cook = <<"см. шаги приготовления">>, 
              steps = [
-                      #step{photo_url = <<"http://www.koolinar.ru/all_image/recipe_pictures/310/310683/rp310683_large.jpg">>,
-                       comment = <<"1. Берем булочки">>},
-                      #step{photo_url = <<"http://teatraditions.ru/upload/medialibrary/34e/70897532_1298108768_63_2.jpg">>,
-                       comment = <<"2. Завариваем чай">>},
-                      #step{photo_url = <<"http://www.fotografiy.net/_ph/131/2/52626611.jpg">>,
-                       comment = <<"3. Ставим булочки и чай на стол">>},
-                      #step{photo_url = <<"http://ic.pics.livejournal.com/pravozaschitneg/45578787/473/473_original.jpg">>,
-                       comment = <<"4. Съешь еще этих мягких французских булок да выпей чаю">>}
+                      #step{photo_url = <<"http://www.koolinar.ru/all_image/recipe_pictures/310/310683/rp310683_large.jpg"/utf8>>,
+                       comment = <<"1. Берем булочки"/utf8>>},
+                      #step{photo_url = <<"http://teatraditions.ru/upload/medialibrary/34e/70897532_1298108768_63_2.jpg"/utf8>>,
+                       comment = <<"2. Завариваем чай"/utf8>>},
+                      #step{photo_url = <<"http://www.fotografiy.net/_ph/131/2/52626611.jpg"/utf8>>,
+                       comment = <<"3. Ставим булочки и чай на стол"/utf8>>},
+                      #step{photo_url = <<"http://ic.pics.livejournal.com/pravozaschitneg/45578787/473/473_original.jpg"/utf8>>,
+                       comment = <<"4. Съешь еще этих мягких французских булок да выпей чаю"/utf8>>}
                      ]}.
