8d7a7fcd22972bd7b886ce1d0b45566c24487602
Dmitry Davydov <haudvd@gmail.com>
2013-11-23 22:49:13 +0400

    sht

--- r_src/algodiet.r
+++ r_src/algodiet.r
@@ -10,7 +10,7 @@ calculatediet <- function(M1, v) {
   sdB = rep(5, length(v))
   
   # solve
-  res <- xsample(A = M1, B = v, G = G, H = H, sdB = sdB)
-  res$X[0, ]
+  res <- xsample(A = M1, B = v, G = G, H = H, sdB = sdB, iter = 2000, burninlength = 1000)
+  res$X[2000, ]
   # rbind(res$X[2000, ], res$X[2500, ], res$X[3000, ], res$X[3200, ], res$X[3500, ], res$X[3800, ], res$X[4000, ])
 }
