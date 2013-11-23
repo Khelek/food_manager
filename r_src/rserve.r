f3dc10657fa0daa9ced26c820a4b92d9306a6dce
Dmitry Davydov <haudvd@gmail.com>
2013-11-23 07:25:58 +0400

    port from r_src

--- r_src/rserve.r
+++ r_src/rserve.r
@@ -2,5 +2,9 @@ require('Rserve')
 source("r_src/algodiet.r")
 
 
-port <- 8085
-run.Rserve(debug = FALSE, port = port, 8085 = paste("--RS-port ", port), config.file = "./rserve.conf")
+
+port = 8085
+if(port == "") {
+  port <- 8085
+} 
+run.Rserve(debug = FALSE, port = port, args = paste("--RS-port ", port), config.file = "./rserve.conf")
