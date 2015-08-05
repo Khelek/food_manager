7d0131b5e0ca7758072c86f5a4bff7e2d4ee986c
Dmitry Davydov <haudvd@gmail.com>
2015-08-06 00:36:12 +0300

    change names

---
+++ other/sql/get_products.sql
@@ -0,0 +1,10 @@
+﻿SELECT prod.name, nutrients.name, info.value
+  FROM (SELECT products._id, products.name FROM products 
+	INNER JOIN products_category ON products_category._id = products.category  
+	WHERE products_category.name = 'Молочные продукты'	
+	AND NOT ARRAY[products.name] && ARRAY['Сыр камамбер, 24%']
+	ORDER BY random() LIMIT 10) as prod
+  INNER JOIN info ON info.product = prod._id
+  INNER JOIN nutrients ON info.nutrient = nutrients._id
+  AND ARRAY[nutrients.name] && ARRAY['Калорийность', 'Стоимость', 'Белки', 'Углеводы', 'Жиры']
+  ORDER BY prod.name;
--- src/http_receiver.erl
+++ src/http_receiver.erl
@@ -19,12 +19,12 @@ handle(Req, State = undefined) ->
     {ok, Req, State}; %ошибка
 handle(Req, State) ->
     {Method, Req2} = cowboy_req:method(Req),
-    ReqResultate = 
-           case Method of 
+    ReqResultate =
+           case Method of
                <<"GET">> ->
                    {Echo, Req3} = cowboy_req:qs_val(<<"lang">>, Req2), %% case lang of ru, en
                    case Echo of
-                       <<"ru">> -> 
+                       <<"ru">> ->
                            Res = db:get_lists(<<"ru">>),
                            ResJson = jsonx:encode(Res),
                            cowboy_req:reply(200, [{<<"content-type">>,<<"application/json; charset=utf-8">>}], ResJson, Req3);
@@ -32,30 +32,30 @@ handle(Req, State) ->
                            Res = db:get_lists(<<"en">>),
                            ResJson = jsonx:encode(Res),
                            cowboy_req:reply(200, [{<<"content-type">>,<<"application/json; charset=utf-8">>}], ResJson, Req3);
-                       _ -> 
+                       _ ->
                            cowboy_req:reply(400, [], <<"Languague is not.">>, Req)
                        end,
                    {ok, Req3, State};
                <<"PUT">> ->
-                   cowboy_req:reply(Req2, cowboy_req:body(infinity, Req2)); %% no 
+                   cowboy_req:reply(Req2, cowboy_req:body(infinity, Req2)); %% no
                <<"POST">> ->
                    {ok, Json, Req3} = cowboy_req:body(infinity, Req2),
-                   Data = decoder(Json), 
+                   Data = decoder(Json),
                    Res = case handle_worker(State, Method, Data) of
                              {ok, ResData} -> #answer{success = true, response = ResData};
                              {error, Reason} -> #answer{success = false, response = Reason}
                          end,
-                   ResJson = encoder(Res), 
+                   ResJson = encoder(Res),
                    cowboy_req:reply(200, [{<<"content-type">>, <<"application/json">>},{<<"charset">>, <<"UTF-8">>}], ResJson, Req3);
                _ ->
-                   some          
+                   some
            end,
     {ok, ReqResultate, State}.
 
 handle_worker(user, <<"POST">>, #auth{login = BinLogin, password = BinPassword, registration = Registration}) ->
     Login = binary:bin_to_list(BinLogin),
     Password = binary:bin_to_list(BinPassword),
-    _Response = case Registration of 
+    _Response = case Registration of
         true ->
             db:new_user(Login, Password);
         false ->
@@ -64,28 +64,28 @@ handle_worker(user, <<"POST">>, #auth{login = BinLogin, password = BinPassword, 
 handle_worker(table, <<"POST">>,  #request{login = Login, action = <<"get">>, body = Data}) ->
     if_registred_do(fun() ->
                             try
-                                {ok, processor:calculate_products_list(Data)} 
-                            catch 
+                                {ok, processor:calculate_products_list(Data)}
+                            catch
                                 throw:Term -> {error, erlang:list_to_bitstring("Не получается найти решение с этими данными")};
                                 exit:Reason -> {error, erlang:list_to_bitstring("Невозможно рассчитать ваше меню с текущими данными")};
                                 error:{badmatch,
-                                       {error,econnrefused}} -> restart_R(), 
+                                       {error,econnrefused}} -> restart_R(),
                                                                 lager:error("R restarted"),
                                                                 {error, erlang:list_to_bitstring("Не получилось рассчитать решение")};
                                 error:Reason -> {error, erlang:list_to_bitstring("Невозможно найти решение с текущими данными")}
                             end
                     end, Login);
 handle_worker(shoplist, <<"POST">>, #request{login = Login, action = <<"get">>}) ->
-    if_registred_do(fun() -> 
-                            db:get_user_shoplist(Login) 
+    if_registred_do(fun() ->
+                            db:get_user_shoplist(Login)
                     end, Login);
 handle_worker(shoplist, <<"POST">>, #request{login = Login, action = <<"set">>, body = Shoplist}) -> %% Shoplist - тупо строка
-    if_registred_do(fun() -> 
-                            db:set_user_shoplist(Login, Shoplist) 
+    if_registred_do(fun() ->
+                            db:set_user_shoplist(Login, Shoplist)
                     end, Login);
 handle_worker(recipes, <<"POST">>, #request{login = Login, action = <<"get">>, body = FindData}) ->
     if_registred_do(fun() ->
-                            recipes:get(FindData) 
+                            recipes:get(FindData)
                     end, Login).
 
 terminate(_Reason, _Req, _State) ->
@@ -128,4 +128,3 @@ decoder(Json) ->
                              {recipe, record_info(fields, recipe)}],
                             [{format, proplist}]),
     Decoder(Json).
-
